29347a8720e9579cc421406d0c59b0b50a3805d4
Leonardo Lopes <leonardo.lopes@thoughtworks.com>
2020-06-01 19:00:18 -0300

    Changed project folder names

--- src/main/java/com/biblioteca/controller/HelloController.java
+++ src/main/java/com/biblioteca/controller/HelloController.java
@@ -1,4 +1,4 @@
-package fasting;
+package com.biblioteca.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
